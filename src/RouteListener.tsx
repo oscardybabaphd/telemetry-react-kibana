16f2e77c17f326090e55d02b1dde20d6cb4e686a
Paul John <Paul.John@unitedcapitalplcgroup.com>
2024-12-12 12:01:31 +0100

    Adjustment to RouteListener file to accommodate other react frameworks and plan js, also making peer Dependencies to avoid version conflict

--- src/RouteListener.tsx
+++ src/RouteListener.tsx
@@ -1,11 +1,46 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { logEvent } from './instrumentation';
+// import React, { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { logEvent } from './instrumentation';
+
+// export const RouteListener: React.FC = () => {
+//   const location = useLocation();
+//   useEffect(() => {
+//     logEvent('navigation', { path: location.pathname });
+//   }, [location.pathname]);
+//   return null;
+// };
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useLocation } from "react-router-dom";
+import { logEvent } from "./instrumentation";
+
+const isClient = typeof window !== "undefined";
 
 export const RouteListener: React.FC = () => {
-  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState<string>("");
+
+  const nextRouter = isClient && useRouter ? useRouter() : null;
+  const reactRouterLocation = isClient && useLocation ? useLocation() : null;
+
   useEffect(() => {
-    logEvent('navigation', { path: location.pathname });
-  }, [location.pathname]);
+    if (isClient) {
+      let pathname: string;
+
+      if (reactRouterLocation) {
+        pathname = reactRouterLocation.pathname;
+      } else if (nextRouter) {
+        pathname = nextRouter.pathname;
+      } else {
+        pathname = window.location.pathname;
+      }
+
+      if (pathname !== currentPath) {
+        setCurrentPath(pathname);
+        logEvent("navigation", { path: pathname });
+      }
+    }
+  }, [nextRouter?.pathname, reactRouterLocation?.pathname, currentPath]);
+
   return null;
 };
